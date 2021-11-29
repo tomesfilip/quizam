c9c98dfce56921939d304e26cec54f229b62c216
f_tomes <f_tomes@utb.cz>
2021-11-29 21:05:01 +0100

    added viewmodel implementation for game start

---
+++ app/src/main/java/com/example/quizam_/presentation/game_start/GameStartEvent.kt
@@ -0,0 +1,6 @@
+package com.example.quizam_.presentation.game_start
+
+sealed class GameStartEvent {
+    data class EnteredUserName(val value: String) : GameStartEvent()
+    object SaveUser: GameStartEvent()
+}
--- app/src/main/java/com/example/quizam_/presentation/game_start/GameStartViewModel.kt
+++ app/src/main/java/com/example/quizam_/presentation/game_start/GameStartViewModel.kt
@@ -1,14 +1,59 @@
 package com.example.quizam_.presentation.game_start
 
+import android.util.Log
+import android.util.Log.DEBUG
+import android.util.Log.INFO
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.quizam_.BuildConfig.DEBUG
 import com.example.quizam_.data.repository.UserRepositoryImpl
+import com.example.quizam_.domain.model.User
+import com.example.quizam_.domain.use_case.UserUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import java.lang.Exception
 import javax.inject.Inject
 
 
 @HiltViewModel
 class GameStartViewModel @Inject constructor(
-    private val userRepositoryImpl: UserRepositoryImpl
+    private val userUseCases: UserUseCases
 ) : ViewModel() {
 
+    private val _userName = mutableStateOf("")
+    val userName: State<String> = _userName
+
+    private val _eventFlow = MutableSharedFlow<UiEvent>()
+    val eventFlow = _eventFlow.asSharedFlow()
+
+    fun onEvent(event: GameStartEvent) {
+        when (event) {
+            is GameStartEvent.EnteredUserName -> {
+                _userName.value = userName.value
+            }
+            is GameStartEvent.SaveUser -> {
+                viewModelScope.launch {
+                    try {
+                        userUseCases.insertUser(
+                            User(
+                                userName = userName.value,
+                                userScore = 0
+                            )
+                        )
+                        _eventFlow.emit(UiEvent.SaveUser)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+        }
+    }
+
+    sealed class UiEvent {
+        object SaveUser: UiEvent()
+    }
 }
